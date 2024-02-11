c29bca05179b4f12b4d0794900be373fd198282c
FrankDena <116834956+FrankDena@users.noreply.github.com>
2024-02-11 18:33:06 +0100

    fix versione docker hub

--- push-docker-hub.sh
+++ push-docker-hub.sh
@@ -1,23 +1,22 @@
 #BUILD SERVICES
-docker build -t  order-service:1.2 ./order-service
-docker build -t  order-validation-service:1.2 ./order-validation-service
-docker build -t  product-service:1.2 ./product-service
-docker build -t  api-gateway:1.2 ./api-gateway
+docker build -t  order-service:1.3 ./order-service
+docker build -t  order-validation-service:1.3 ./order-validation-service
+docker build -t  product-service:1.3 ./product-service
+docker build -t  api-gateway:1.3 ./api-gateway
 
 #PUSH TO DOCKER HUB
 docker login
 
-docker tag order-service:1.2 haowe/asw-ordermanager:order-service-1.2
-docker push haowe/asw-ordermanager:order-service-1.2
+docker tag order-service:1.3 haowe/asw-ordermanager:order-service-1.3
+docker push haowe/asw-ordermanager:order-service-1.3
 
-docker tag product-service:1.2 haowe/asw-ordermanager:product-service-1.2
-docker push haowe/asw-ordermanager:product-service-1.2
+docker tag product-service:1.3 haowe/asw-ordermanager:product-service-1.3
+docker push haowe/asw-ordermanager:product-service-1.3
 
-docker tag order-validation-service:1.2 haowe/asw-ordermanager:order-validation-service-1.2
-docker push haowe/asw-ordermanager:order-validation-service-1.2
+docker tag order-validation-service:1.3 haowe/asw-ordermanager:order-validation-service-1.3
+docker push haowe/asw-ordermanager:order-validation-service-1.3
 
-docker tag api-gateway:1.2 haowe/asw-ordermanager:api-gateway-1.2
-docker push haowe/asw-ordermanager:api-gateway-1.2
-
+docker tag api-gateway:1.3 haowe/asw-ordermanager:api-gateway-1.3
+docker push haowe/asw-ordermanager:api-gateway-1.3
 
 echo ALL DONE!
